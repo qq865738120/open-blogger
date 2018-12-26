f58ea3acc9f8ffbb0a5e7f8279ea87746d25d914
qq865738120 <zwj865738120@163.com>
2018-12-26 19:28:03 +0800

    v1.0.0

--- components/MainDisplay.vue
+++ components/MainDisplay.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
-    <nuxt-link :to="{ name: 'article-id', params: { id: 123 }}">
+    <nuxt-link
+      :to="{ name: 'article-id', params: { id: 123 }}"
+      class="content">
       <img
         v-lazy="'https://static001.infoq.cn/resource/image/ea/74/ea1ed1fe11dc85f2ffb37dda5c2b4574.jpg?x-oss-process=image/crop,y_145,w_999,h_514/resize,w_776,h_400'"
         class="img"
       >
+      <div class="content-right margin-left-30">
+        <span>123</span>
+      </div>
     </nuxt-link>
   </div>
 </template>
@@ -13,7 +18,17 @@
 export default {}
 </script>
 
-<style>
+<style lang="scss">
+@import 'assets/style/common';
+
+.content {
+  width: 100%;
+  display: flex;
+}
+.content-right > span:nth-child(1) {
+  color: $--color-14;
+  font-size: $--large;
+}
 .img {
   width: 388px;
   height: 200px;
