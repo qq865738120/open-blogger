72f3b0dfdf34a71953ad99f3b1ecac64152fc333
qq865738120 <zwj865738120@163.com>
2018-12-28 23:19:56 +0800

    v1.0.0

--- pages/index.vue
+++ pages/index.vue
@@ -24,6 +24,8 @@
     class="flex-inline-between"
     style="width: 1080px">
     <div style="width: 712px">
+
+      <!-- 精选内容start -->
       <el-card class="max-box-card">
         <div
           slot="header"
@@ -40,22 +42,39 @@
             :data="item" />
         </div>
       </el-card>
+      <!-- 精选内容end -->
+
+      <!-- 推荐内容start -->
       <el-card class="max-box-card margin-top-20">
         <div
           slot="header"
           class="clearfix">
           <span class="iconfont open-yuedu font-extra-large">{{ $t('homePage.recommend') }}</span>
         </div>
+        <div
+          v-for="(item, index) of baseDisplay"
+          :key="index"
+          class="margin-bottom-20">
+          <base-display :data="item" />
+        </div>
       </el-card>
+      <!-- 推荐内容end -->
+
     </div>
     <div style="width: 344px;">
+
+      <!-- 热点start -->
       <el-card class="max-box-card">
         <div
           slot="header"
           class="clearfix">
           <span class="iconfont open-neifenmike font-extra-large">{{ $t('homePage.hotspot') }}</span>
         </div>
+        <div>
+          <hot-list />
+        </div>
       </el-card>
+      <!-- 热点end -->
     </div>
   </div>
 </template>
@@ -63,11 +82,15 @@
 <script>
 import MainDisplay from '~/components/MainDisplay.vue'
 import SubDisplay from '~/components/SubDisplay.vue'
+import BaseDisplay from '~/components/BaseDisplay.vue'
+import HotList from '~/components/HotList.vue'
 
 export default {
   components: {
     MainDisplay,
-    SubDisplay
+    SubDisplay,
+    BaseDisplay,
+    HotList
   },
   data() {
     return {
@@ -101,6 +124,48 @@ export default {
           author: '覃云，徐川',
           tag: ['区块链', 'web']
         }
+      ],
+      baseDisplay: [
+        {
+          imgSrc:
+            'https://static001.infoq.cn/resource/image/ea/74/ea1ed1fe11dc85f2ffb37dda5c2b4574.jpg?x-oss-process=image/crop,y_145,w_999,h_514/resize,w_776,h_400',
+          title: '2018 年终盘点：“年度爆款”的区块链真的结束了吗？',
+          describe: '年终盘点之区块链：拨霾见日，大路朝天',
+          author: '覃云，徐川',
+          tag: ['区块链', 'web']
+        },
+        {
+          imgSrc:
+            'https://static001.infoq.cn/resource/image/ea/74/ea1ed1fe11dc85f2ffb37dda5c2b4574.jpg?x-oss-process=image/crop,y_145,w_999,h_514/resize,w_776,h_400',
+          title: '2018 年终盘点：“年度爆款”的区块链真的结束了吗？',
+          describe: '年终盘点之区块链：拨霾见日，大路朝天',
+          author: '覃云，徐川',
+          tag: ['区块链', 'web']
+        },
+        {
+          imgSrc:
+            'https://static001.infoq.cn/resource/image/ea/74/ea1ed1fe11dc85f2ffb37dda5c2b4574.jpg?x-oss-process=image/crop,y_145,w_999,h_514/resize,w_776,h_400',
+          title: '2018 年终盘点：“年度爆款”的区块链真的结束了吗？',
+          describe: '年终盘点之区块链：拨霾见日，大路朝天',
+          author: '覃云，徐川',
+          tag: ['区块链', 'web']
+        },
+        {
+          imgSrc:
+            'https://static001.infoq.cn/resource/image/ea/74/ea1ed1fe11dc85f2ffb37dda5c2b4574.jpg?x-oss-process=image/crop,y_145,w_999,h_514/resize,w_776,h_400',
+          title: '2018 年终盘点：“年度爆款”的区块链真的结束了吗？',
+          describe: '年终盘点之区块链：拨霾见日，大路朝天',
+          author: '覃云，徐川',
+          tag: ['区块链', 'web']
+        },
+        {
+          imgSrc:
+            'https://static001.infoq.cn/resource/image/ea/74/ea1ed1fe11dc85f2ffb37dda5c2b4574.jpg?x-oss-process=image/crop,y_145,w_999,h_514/resize,w_776,h_400',
+          title: '2018 年终盘点：“年度爆款”的区块链真的结束了吗？',
+          describe: '年终盘点之区块链：拨霾见日，大路朝天',
+          author: '覃云，徐川',
+          tag: ['区块链', 'web']
+        }
       ]
     }
   }
@@ -112,5 +177,8 @@ export default {
 
 .max-box-card {
   width: 100%;
+}
+.max-box-card div:last-of-type {
+  @include spacing(margin, bottom, 0);
 }
 </style>
